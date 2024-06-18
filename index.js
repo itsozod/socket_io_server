695fba6187005eea3c084d5a1899f21ef246e6b0
Ozod <o.bukhorizoda@mail.ru>
2024-06-18 13:09:51 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+    socket.broadcast.emit("notify", data);
+  });
+});
+
+server.listen("3002", () => {
+  console.log("Server is running");
+});
